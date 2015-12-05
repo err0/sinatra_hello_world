71fb36d1d5e220339fd67b6fe5001f95d5a41515
Erik Rogneby <erogneby@gmail.com>
2015-12-04 22:15:39 -0500

    marking the branch in the text file.

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -6,6 +6,6 @@ system('bundle install --deployment')
 
 # create text file for testing
 textfile = open('before_restart.txt', 'w')
-textfile.puts('this is before restart')
+textfile.puts('this is before restart - on branch foo')
 textfile.close
 
